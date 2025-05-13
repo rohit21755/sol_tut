2d79d7387e8459546aa76444b68745fc00b885dc
rohit21755 <rohit.k09022002@gmail.com>
2025-05-13 18:39:42 +0530

    nft_decode added

---
+++ nft_decoded/index.ts
@@ -0,0 +1,28 @@
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
+import * as borsh from '@coral-xyz/borsh'
+const tokenMint = new PublicKey("SMBtHCCC6RYRutFEPb4gZqeBLUZbMNhRKaMKZZLHi7W")
+const programId = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
+
+const seeds = [Buffer.from("metadata"), programId.toBytes(), tokenMint.toBytes()]
+
+const [metadataPDA, bump] = PublicKey.findProgramAddressSync(seeds, programId)
+
+const connection = new Connection(clusterApiUrl("mainnet-beta"))
+
+async function main(){
+    const accountInfo = await connection.getAccountInfo(metadataPDA)
+    console.log(accountInfo?.data)
+    const borshMetaDataLayout = borsh.struct([
+        borsh.u8("key"),
+        borsh.publicKey("updateAuthority"),
+        borsh.publicKey("mint"),
+        borsh.str("name"),
+        borsh.str("symbol"),
+        borsh.str("uri")
+    ])
+    if (accountInfo) {
+        const metadata = borshMetaDataLayout.decode(accountInfo.data)
+        console.log(metadata)
+    }
+}
+main()
