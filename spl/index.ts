e3b2a865db98c8dc8854e748a9331c144d56a0cd
rohit21755 <rohit.k09022002@gmail.com>
2025-05-13 23:04:06 +0530

    new function added

--- spl/index.ts
+++ spl/index.ts
@@ -1,1 +1,37 @@
-console.log("hello")
+import { createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint, getOrCreateAssociatedTokenAccount, MINT_SIZE, mintTo, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+    const connetion = new Connection(clusterApiUrl("devnet"))   
+    const secret = new Uint8Array([115,123,87,5,10,234,145,243,237,56,54,152,136,68,142,143,220,130,140,225,248,40,103,236,212,23,195,18,236,17,45,42,186,52,60,12,249,181,59,204,142,140,142,186,234,134,253,187,39,208,104,59,17,138,38,242,233,6,63,178,63,104,217,212])
+    const payer = Keypair.fromSecretKey(secret)
+// async function main(){
+
+//     const mint = await createMint(connetion,payer,payer.publicKey,payer.publicKey,6)
+//     console.log(mint.toBase58())
+//     const tokenAccount = await getOrCreateAssociatedTokenAccount(connetion, payer, mint, payer.publicKey)
+//     const sx = await mintTo(connetion, payer, mint, tokenAccount.address, payer, 100)
+//     console.log(sx)
+// }
+// main()
+
+async function main() {
+    const lamports = await getMinimumBalanceForRentExemptMint(connetion)
+    const mintAccount = Keypair.generate()
+    const ix1 = SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: mintAccount.publicKey,
+        lamports: lamports,
+        space: MINT_SIZE,
+        programId: TOKEN_PROGRAM_ID
+    })
+
+    const ix2 = createInitializeMint2Instruction(mintAccount.publicKey, 6, payer.publicKey, payer.publicKey)
+
+    const tx = new Transaction().add(ix1, ix2)
+
+    const  sx = await sendAndConfirmTransaction(connetion, tx, [payer, mintAccount])
+    console.log(sx)
+
+    // new Promise(r => setTimeout(r, 2000))
+}
+
+main()
